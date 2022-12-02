076a8bf087c6d36f10e66b4f2d86fb1493207d53
Noah.G <noahgerber100@gmail.com>
2022-12-02 07:09:26 +0100

    Day2 cleanup

--- src/Day2.java
+++ src/Day2.java
@@ -9,25 +9,10 @@ public class Day2 extends Day
         int points = 0;
         for (String s : input)
         {
-            int enemy = switch (s.split(" ")[0])
-                    {
-                        case "A" -> 0;  //rock
-                        case "B" -> 1;  //paper
-                        case "C" -> 2;  //cicors
-                        default -> -1;
-                    };
-            int own = switch (s.split(" ")[1])
-                    {
-                        case "X" -> 0;  //rock      //lose  r->c, p->r, c->p
-                        case "Y" -> 1;  //paper     //draw  r->r, p->p, c->c
-                        case "Z" -> 2;  //cicors    //win   r->p, p->c, c->r
-                        default -> -1;
-                    };
-            //a)
-            //points += own + 1 + new int[]{3, 6, 0}[(own - enemy + 3)%3];
-
-            //b)
-            points += own * 3 + (enemy + own + 2) % 3 + 1;
+            int enemy = "ABC".indexOf(s.split(" ")[0]);
+            int own = "XYZ".indexOf(s.split(" ")[1]);
+            //points += own + 1 + new int[]{3, 6, 0}[(own - enemy + 3)%3];  //a
+            points += own * 3 + (enemy + own + 2) % 3 + 1;                  //b
         }
         System.out.println(points);
     }
