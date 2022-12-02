6ca0dc681e3129adb5214c53b5ed4efef3631c78
Noah.G <noahgerber100@gmail.com>
2022-12-02 07:01:08 +0100

    day2 clean

--- src/Day2.java
+++ src/Day2.java
@@ -9,100 +9,25 @@ public class Day2 extends Day
         int points = 0;
         for (String s : input)
         {
-            String enemy = s.split(" ")[0];
-            String hasTo = s.split(" ")[1];
-            String own = "";
-            switch (hasTo)
-            {
-                case "X":
-                    switch (enemy)
+            int enemy = switch (s.split(" ")[0])
                     {
-                        case "A":
-                            own = "Z";
-                            break;
-                        case "B":
-                            own = "X";
-                            break;
-                        case "C":
-                            own = "Y";
-                            break;
-                    }
-                    break;
-                case "Y":
-                    switch (enemy)
+                        case "A" -> 0;  //rock
+                        case "B" -> 1;  //paper
+                        case "C" -> 2;  //cicors
+                        default -> -1;
+                    };
+            int own = switch (s.split(" ")[1])
                     {
-                        case "A":
-                            own = "X";
-                            break;
-                        case "B":
-                            own = "Y";
-                            break;
-                        case "C":
-                            own = "Z";
-                            break;
-                    }
-                    break;
-                case "Z":
-                    switch (enemy)
-                    {
-                        case "A":
-                            own = "Y";
-                            break;
-                        case "B":
-                            own = "Z";
-                            break;
-                        case "C":
-                            own = "X";
-                            break;
-                    }
-                    break;
-            }
-            //calc points
-            switch (own)
-            {
-                case "X":
-                    points += 1;
-                    switch (enemy)
-                    {
-                        case "A":
-                            points += 3;
-                            break;
-                        case "B":
-                            break;
-                        case "C":
-                            points += 6;
-                            break;
-                    }
-                    break;
-                case "Y":
-                    points += 2;
-                    switch (enemy)
-                    {
-                        case "A":
-                            points += 6;
-                            break;
-                        case "B":
-                            points += 3;
-                            break;
-                        case "C":
-                            break;
-                    }
-                    break;
-                case "Z":
-                    points += 3;
-                    switch (enemy)
-                    {
-                        case "A":
-                            break;
-                        case "B":
-                            points += 6;
-                            break;
-                        case "C":
-                            points += 3;
-                            break;
-                    }
-                    break;
-            }
+                        case "X" -> 0;  //rock      //lose  r->c, p->r, c->p
+                        case "Y" -> 1;  //paper     //draw  r->r, p->p, c->c
+                        case "Z" -> 2;  //cicors    //win   r->p, p->c, c->r
+                        default -> -1;
+                    };
+            //a)
+            //points += own + 1 + new int[]{3, 6, 0}[(own - enemy + 3)%3];
+
+            //b)
+            points += own * 3 + (enemy + own + 2) % 3 + 1;
         }
         System.out.println(points);
     }
