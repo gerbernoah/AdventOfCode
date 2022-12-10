02b043b5f46c66b402d86455efe3eb4ea4fc785e
Noah.G <noahgerber100@gmail.com>
2022-12-10 05:14:16 +0100

    setup Day10

--- src/Day.java
+++ src/Day.java
@@ -27,6 +27,6 @@ public abstract class Day
 
     public static void main(String[] args)
     {
-        new Day9();
+        new Day10();
     }
 }
---
+++ src/Day10.java
@@ -0,0 +1,24 @@
+public class Day10 extends Day
+{
+    @Override
+    protected Object part1()
+    {
+        int result = 0;
+        for (String s : input)
+        {
+
+        }
+        return result;
+    }
+
+    @Override
+    protected Object part2()
+    {
+        int result = 0;
+        for (String s : input)
+        {
+
+        }
+        return result;
+    }
+}
