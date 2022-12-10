d8c4fbc6cdd0e3f4b797eb296e524feb28a29312
Noah.G <noahgerber100@gmail.com>
2022-12-10 05:13:18 +0100

    CleanUp Day6

--- src/Day6.java
+++ src/Day6.java
@@ -4,22 +4,12 @@ import java.util.Arrays;
 public class Day6 extends Day
 {
     @Override
-    protected Object part1()
-    {
-        char[] chars = input.get(0).toCharArray();
-        ArrayList<Character> last = new ArrayList<>();
-        for (int i = 0; i < chars.length; i++)
-        {
-            int d = 1 + last.lastIndexOf(chars[i]);
-            while (d-->0) last.remove(d);
-            last.add(chars[i]);
-            if (last.size() == 4) return i+1;
-        }
-        return -1;
-    }
+    protected Object part1(){ return findFirstMarker(4);}
 
     @Override
-    protected Object part2()
+    protected Object part2(){ return findFirstMarker(14);}
+
+    private int findFirstMarker(int seq)
     {
         char[] chars = input.get(0).toCharArray();
         ArrayList<Character> last = new ArrayList<>();
@@ -28,7 +18,7 @@ public class Day6 extends Day
             int d = 1 + last.lastIndexOf(chars[i]);
             while (d-->0) last.remove(d);
             last.add(chars[i]);
-            if (last.size() == 14) return i+1;
+            if (last.size() == seq) return i+1;
         }
         return -1;
     }
