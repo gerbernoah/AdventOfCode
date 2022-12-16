8d6dfcdc6edf68d991cb87c85819a22e17979853
Noah.G <noahgerber100@gmail.com>
2022-12-16 13:26:14 +0100

    setup Day16

--- src/Day.java
+++ src/Day.java
@@ -27,6 +27,6 @@ public abstract class Day
 
     public static void main(String[] args)
     {
-        new Day15();
+        new Day16();
     }
 }
---
+++ src/Day16.java
@@ -0,0 +1,14 @@
+public class Day16 extends Day
+{
+    @Override
+    protected Object part1()
+    {
+        return null;
+    }
+
+    @Override
+    protected Object part2()
+    {
+        return null;
+    }
+}
